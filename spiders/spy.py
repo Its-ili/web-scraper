2b56c0449e7eb61e50bee562c8f26a61582f096b
Its-ili <118632534+Its-ili@users.noreply.github.com>
2023-04-17 21:27:39 +0330

    Update spy.py

--- spiders/spy.py
+++ spiders/spy.py
@@ -1,1 +1,12 @@
+import scrapy
 
+class MySpider(scrapy.Spider):
+    name = 'spy'
+    start_urls = ['http://example.com']
+
+    def parse(self, response):
+        # Extract data from the webpage using XPath or CSS selectors
+        data = {}
+        data['title'] = response.css('h1::text').get()
+        data['content'] = response.css('p::text').getall()
+        yield data
